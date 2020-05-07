83e0e936ba60aafc33201dd5519f36cae021c513
miyeung2 <36974942+miyeung2@users.noreply.github.com>
2020-05-07 15:45:17 -0700

    minor changes for perfection

--- main.js
+++ main.js
@@ -29,24 +29,68 @@ function getParameterByName(name, url) {
   };
   function formattedString(value) {
     console.log(value)
-    return value.split(" , ").map(function(link) { return `<a href="${link}">Buy here! </a> ` }).join("");
+    return value.split(" , ").map(function(link) { return `<a href="${link}">Buy here! </a> ` }).join();
   }
 
   var listView = function(id, games, images, logos, storeLinks) {
     return ` <div class="col-sm-4">
       <div class="card" >
-      ${images ? `<img src="${images[0].url}"  class="card-img-top">`  : ``}
+      <a href="index.html?id=${id}"> ${images ? `<img src="${images[0].url}"  class="card-img-top">`  : ``}</a>
   <div class="card-body">
     <h5 class="card-title">${games}</h5>
-    
-    ${logos[0] ? logos.map(function(logo) { return `<img src="${logo.url}" class="logo">`} ) : ``}
-  
-    <p class="card-text">${formattedString(storeLinks)}</p>
+    <h6 class="card-text">Click on the image for more info!</h6>
+
   </div>
 </div> </div>
     `;
   };
+  var getOneRecord = function(id) {
+    $.getJSON(
+      `https://api.airtable.com/v0/appvhXT17EwqV7ELl/Games/${id}?api_key=keysRd14BRX8ndZJX`,
+      function(record) {
+        var html = [];
+        var games = record.fields["Games"];
+        var images = record.fields["Images"];
+        var logos = record.fields["Console"];
+        var price = record.fields["Price"];
+        var playersRange = record.fields["Players range"];
+        var storeLinks = record.fields["Store Links"]
+        var description = record.fields["Description"]
+        html.push(
+          detailView(
+            games, images, logos, price, playersRange,storeLinks, description
+          )
+        );
+        $(".detail-view").append(html);
+      }
+    );
+  };
 
+  var detailView = function(
+    games, images, logos, price, playersRange, storeLinks, description
+  ) {
+    return `
+    <div class="card mb-3";>
+    <div class="row no-gutters">
+      <div class="col-md-4">
+      ${images ? `<img src="${images[0].url}"  class="card-img">`  : ``}
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${games}</h5>
+          <p class="card-text">${description}</p>
+          <p class="card-text">Available for these consoles only:</p>
+          <div class="space1">${logos[0] ? logos.map(function(logo) { return `<img src="${logo.url}" class="logo">`} ) : ``}</div>
+          <p class="card-text space">${formattedString(storeLinks)}</p>
+          <p class="card-text">$${price} without tax</p>
+          <p class="card-text">${playersRange} players</p>
+          
+          </div>
+      </div>
+    </div>
+  </div>
+    `;
+  };
   var id = getParameterByName("id");
   if (id) {
     getOneRecord(id);
